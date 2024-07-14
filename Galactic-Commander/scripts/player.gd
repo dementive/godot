64ee4892170abaad3f37d1365e2c41915273f969
dementive <87823030+dementive@users.noreply.github.com>
2024-07-13 20:45:09 -0400

    Camera and ship model fixes

--- Galactic-Commander/scripts/player.gd
+++ Galactic-Commander/scripts/player.gd
@@ -6,54 +6,53 @@ extends CharacterBody3D
 @export var enable_rotation = true;
 @export var enable_movement = true;
 
-@onready var ship_body : Node3D = $Destroyer_01.get_child(0)
+@onready var ship_body : Node3D = $ShipBody;
 const BACKWARD_RATIO = 0.5;
-var fast_as_fuck: bool = true
 
 var speed = 0;
 var drift_direction = Vector3.FORWARD;
+	
 
 func _process(delta):
-	# Check if the mouse is inside the game window
 	var viewport = get_viewport()
 	var mouse_position = viewport.get_mouse_position()
 	var is_mouse_inside = mouse_position.x >= 0 and mouse_position.y >= 0 and mouse_position.x <= viewport.size.x and mouse_position.y <= viewport.size.y
 	
 	if enable_rotation and is_mouse_inside:
 		# Rotate Player
-		var relative_mouse = _get_relative_mouse()
-		relative_mouse.y *= -1
-		var player_basis = transform.basis
-		player_basis = player_basis.rotated(player_basis.x, relative_mouse.y * rotation_speed * delta)
-		player_basis = player_basis.rotated(player_basis.y, -relative_mouse.x * rotation_speed * delta)
-		player_basis = player_basis.orthonormalized()
-		transform.basis = player_basis
+		var relative_mouse = _get_relative_mouse();
+		var basis = transform.basis;
+		basis = basis.rotated(basis.x, relative_mouse.y * rotation_speed * delta);
+		basis = basis.rotated(basis.y, -relative_mouse.x * rotation_speed * delta);
+		basis = basis.orthonormalized();
+		transform.basis = basis;
 		
 		# Rotate Ship
-		var ship_basis = Basis.IDENTITY
-		var ship_scale = ship_body.scale
-		ship_basis = ship_basis.rotated(Vector3.UP, -relative_mouse.x * rotation_speed / 2.0)
-		ship_basis = ship_basis.rotated(Vector3.RIGHT, relative_mouse.y * rotation_speed)
-		ship_basis = ship_basis.rotated(ship_basis.z, relative_mouse.x * rotation_speed)
-		ship_basis = ship_basis.orthonormalized()
-		ship_body.basis = ship_basis
-		ship_body.scale = ship_scale
+		var ship_basis = Basis.IDENTITY;
+		var scale = ship_body.scale;
+		ship_basis = ship_basis.rotated(Vector3.UP, -relative_mouse.x * rotation_speed / 2.0);
+		ship_basis = ship_basis.rotated(Vector3.RIGHT, relative_mouse.y * rotation_speed);
+		ship_basis = ship_basis.rotated(ship_basis.z, relative_mouse.x * rotation_speed);
+		ship_basis = ship_basis.orthonormalized();
+		ship_body.basis = ship_basis;
+		ship_body.scale = scale;
 	
 	if enable_movement:
-		_move_forward(delta)
-
+		_move_forward(delta);
+	
 func _move_forward(delta):
-	var forward = ship_body.global_transform.basis.x.normalized();
+	var forward = ship_body.global_transform.basis.z.normalized();
 	if Input.is_action_pressed("ui_up"):
-		speed = min(speed - acceleration * delta, max_speed);
+		speed = min(speed + acceleration * delta, max_speed);
 	elif Input.is_action_pressed("ui_down"):
-		speed = max(speed + acceleration * delta, -max_speed * BACKWARD_RATIO);
+		speed = max(speed - acceleration * delta, -max_speed * BACKWARD_RATIO);
 	else:
 		speed -= speed * delta;
 	
 	velocity = forward * speed;
-	if fast_as_fuck:
-		velocity *= 50.0
+	
+	velocity *= 150
+	
 	move_and_slide();
 
 # Get a vector for the mouse relative to the center of the screen
